8ad21e1706d51cec3a96c3c3a65b8c9e0a596fad
Eric O'Connell <eric@zerominuszero.net>
2012-11-04 13:35:15 -0800

    server-side rendering is alive

--- js/server.js
+++ js/server.js
@@ -2,11 +2,29 @@ var jquery = require('jquery');
 var jsdom = require('jsdom');
 var requirejs = require('requirejs');
 
-var document = jsdom.jsdom();
-var window = document.createWindow();
-
 var express = require('express');
 var app = express();
+
+app.use('/js', express.directory('js'));
+app.use('/js', express.static('js'));
+
+var fs = require('fs');
+var html = fs.readFileSync('index.html').toString();
+var jq = fs.readFileSync('js/lib/jquery.min.js').toString();
+
+var window, document, $;
+
+jsdom.env({
+    html: html,
+    src: [jq],
+    done: function(err, win) {
+        window = win;
+        document = window.document;
+        $ = window.jQuery;
+    }
+});
+
+
 
 requirejs.config({
     nodeRequire: require,
@@ -40,14 +58,23 @@ requirejs.config({
     }
 });
 
-requirejs(['app/views/schedule', 'app/data'], function(sv, data) {
+
+requirejs(['backbone', 'app/views/schedule', 'app/data'], function(Backbone, sv, data) {
+    Backbone.setDomLibrary($);
+    Backbone.View.prototype.make = function(tagName, attributes, content) {
+        var el = document.createElement(tagName);
+        if (attributes) $(el).attr(attributes);
+        if (content) $(el).html(content);
+        return el;
+    };
+
     var schedule = new sv({
         schedule: data.Marjorie.schedule(),
         vent: { on: function() {} }
     });
     app.get('/', function(req, res) {
         schedule.render();
-        res.send(schedule.el.innerHTML);
+        res.send(document.documentElement.innerHTML);
     });
 });
 
